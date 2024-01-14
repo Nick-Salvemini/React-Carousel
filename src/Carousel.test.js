fee4b5ba5ab21c2bf9bf37b33f987f2a353b95c1
Nick Salvemini <salvemini.nick@gmail.com>
2024-01-14 17:00:21 -0500

    completed

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,12 +2,17 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
-it("works when you click on the right arrow", function() {
-  const { queryByTestId, queryByAltText } = render(<Carousel />);
+it('renders without crashing', () => {
+  render(<Carousel />)
+})
 
-  // expect the first image to show, but not the second
-  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
-  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+it("matches snapshot", function () {
+  const { asFragment } = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("works when you click on the right and left arrows", function () {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
 
   // move forward in the carousel
   const rightArrow = queryByTestId("right-arrow");
@@ -16,4 +21,30 @@ it("works when you click on the right arrow", function() {
   // expect the second image to show, but not the first
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
 });
+
+it("removes the left arrow on the first image and right arrow on the last image", function () {
+  const { getByTestId } = render(<Carousel />);
+  const leftArrow = getByTestId("left-arrow");
+  const rightArrow = getByTestId("right-arrow");
+
+  // left arrow is not available on the first image
+  expect(leftArrow).toHaveClass('hidden');
+  expect(rightArrow).not.toHaveClass('hidden');
+
+  // move forward in the carousel twice
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // left arrow is not available on the first image
+  expect(leftArrow).not.toHaveClass('hidden');
+  expect(rightArrow).toHaveClass('hidden');
+})
